7bd0f4f9a8d01187a571948c532225e4c8e74c1c
Nabil Birgle <nabil.birgle@gmail.com>
2024-04-19 19:17:26 +0200

    Initial Commit

---
+++ PI-gui/PI_guiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PI_guiApp.swift
+//  PI-gui
+//
+//  Created by Nabil Birgle on 19/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct PI_guiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
